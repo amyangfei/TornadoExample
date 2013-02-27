ff4e0021b9616db2371a371fe0b5b0ac9a5a130e
amyangfei <amyangfei@gmail.com>
2013-02-27 12:12:11 +0800

    db dump

---
+++ data/Dump20130227.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `yuedu` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `yuedu`;
+-- MySQL dump 10.13  Distrib 5.5.16, for osx10.5 (i386)
+--
+-- Host: localhost    Database: yuedu
+-- ------------------------------------------------------
+-- Server version	5.5.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `y_users`
+--
+
+DROP TABLE IF EXISTS `y_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `y_users` (
+  `u_id` int(11) NOT NULL AUTO_INCREMENT,
+  `u_email` varchar(45) NOT NULL,
+  `u_name` varchar(45) DEFAULT 'Reader',
+  `u_password` varchar(45) NOT NULL,
+  PRIMARY KEY (`u_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `y_users`
+--
+
+LOCK TABLES `y_users` WRITE;
+/*!40000 ALTER TABLE `y_users` DISABLE KEYS */;
+INSERT INTO `y_users` VALUES (1,'admin@gmail.com','hello','a7ea773036a99a6130e41e8c0b65a1b0e3616def');
+/*!40000 ALTER TABLE `y_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-02-27 12:10:17
